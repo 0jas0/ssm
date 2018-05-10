f718bc72c0b1cb09899dcf53668540f98dd811da
琚安生 <juansheng@qijiayoudao.com>
2018-05-10 11:07:13 +0800

    add

---
+++ src/main/java/com/jas/web/bean/BaseDO.java
@@ -0,0 +1,41 @@
+package com.jas.web.bean;
+
+public class BaseDO {
+    private Integer id;
+    private Integer isDel;
+    private Long addtime;
+    private Long modtime;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Integer isDel) {
+        this.isDel = isDel;
+    }
+
+    public Long getAddtime() {
+        return addtime;
+    }
+
+    public void setAddtime(Long addtime) {
+        this.addtime = addtime;
+    }
+
+    public Long getModtime() {
+        return modtime;
+    }
+
+    public void setModtime(Long modtime) {
+        this.modtime = modtime;
+    }
+}
---
+++ src/main/java/com/jas/web/bean/StudentDO.java
@@ -0,0 +1,4 @@
+package com.jas.web.bean;
+
+public class StudentDO extends BaseDO {
+}
